f21fa13d3b5095c0d86aa903ab1a96dbe3c26ea5
Namuun0521 <turboldnamuun@gmail.com>
2026-03-10 17:38:50 +0800

    button fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,19 +40,24 @@ export default function RootLayout({
             <div className="flex justify-between items-center p-4 gap-4 h-16">
               <div className="text-2xl font-semibold text-black">Quiz app</div>
             </div>
+            <div className="  ">
+              <SignedOut>
+                <SignInButton>
+                  <button className=" text-black rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer ">
+                    Sign In
+                  </button>
+                </SignInButton>
+                <SignUpButton>
+                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer ">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
 
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </header>
 
           {children}
